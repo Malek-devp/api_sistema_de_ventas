e37316249ead8598e275a00725b4313e4f93d2f4
Malek Herrera <jabescruzdevalera@gmail.com>
2026-07-11 13:33:34 -0500

    refactor: optimización del modulo productos y creación de la carpeta utils, para crear funciones reutilizables y no estar repiendo logica

--- src/controllers/productos.controller.ts
+++ src/controllers/productos.controller.ts
@@ -1,41 +1,44 @@
 import { getProductosDB, postProductosDB, putProductosDB, deleteProductosDB } from '../services/productos.service.js'; // FIX: typo en nombre del archivo
 import type { Request, Response, NextFunction } from "express";
 import type { Producto, CrearProducto } from '../interface/productos.interface.js';
+import {parseId} from '../utils/parseId.js'
 
-export async function getProductos(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+export async function getProductos(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-        const productos:Producto[] = await getProductosDB();
+        const productos = await getProductosDB();
         res.json(productos);
     } catch (error) { 
         next(error);
     }
 }
 
-export async function postProductos(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+export async function postProductos(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-        const body:CrearProducto = req.body;
-        const result:Producto = await postProductosDB(body);
+        const producto:CrearProducto = req.body;
+        const result = await postProductosDB(producto);
         res.json(result);
     } catch (error) {
         next(error);
     } 
 }
 
-export async function putProductos(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+export async function putProductos(req: Request, res: Response, next: NextFunction): Promise<Response|void> {
     try {
-        const {id} = req.params;
-        const body:CrearProducto = req.body;
-        const result:Producto = await putProductosDB(Number(id), body);
+        const id = req.params.id;
+        const idParsing = parseId(id)
+        const producto:CrearProducto = req.body;
+        const result = await putProductosDB(idParsing, producto);
         res.json(result);
     } catch (error) {
         next(error);
     }
 }
 
-export async function deleteProductos(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+export async function deleteProductos(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-        const {id} = req.params;
-        const result:Producto = await deleteProductosDB(Number(id));
+        const id = req.params.id;
+        const idParsing = parseId(id)
+        const result = await deleteProductosDB(idParsing);
         res.json(result);
     } catch (error) {
         next(error);
