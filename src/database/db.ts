b12219ab66b3fec5ed40fbd6d358c5a5b79ba276
Malek Herrera <jabescruzdevalera@gmail.com>
2026-07-28 08:45:06 -0500

    chore: implementación de docker compose

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,7 @@ import {Pool} from 'pg';
 
 const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'postgres',
     database: process.env.DB_DATABASE || 'api_sistema_de_ventas',
     password: process.env.DB_PASSWORD || 'malek2003',
     port: Number(process.env.DB_PORT) || 5432,
